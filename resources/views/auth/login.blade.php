c7cd89215116a80e5bfceec4b0c9ba167ac945b3
chadrack kanza <chadrackkanza2015@gmail.com>
2022-02-01 18:15:03 +0100

    design auth page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,10 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
+<x-app-layout>
+    {{-- <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
         </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
+        
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
@@ -44,5 +37,64 @@
                 </x-jet-button>
             </div>
         </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+    </x-jet-authentication-card> --}}
+    <!-- Sign in form -->
+    <div class="container d-flex flex-wrap justify-content-center  ">
+        <div class="w-100 align-self-end pt-1 pt-md-4 pb-4" style="max-width: 526px;">
+          <h1 class="text-center text-xl-start">Connectez-vous</h1>
+          <p class="text-center text-xl-start pb-3 mb-3">Vous n'avez pas encore de compte? <a href="{{ route('register')}}">Inscrivez-vous ici.</a></p>
+          <x-jet-validation-errors class="mb-4 text-danger" />
+
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-primary">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+          <form class="needs-validation mb-2"  method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="position-relative mb-4">
+              <label for="email" class="form-label fs-base">Email</label>
+              <input type="email" id="email" class="form-control form-control-lg" name="email" :value="old('email')" required>
+              <div class="invalid-feedback position-absolute start-0 top-100">Please enter a valid email address!</div>
+            </div>
+            <div class="mb-4">
+              <label for="password" class="form-label fs-base">{{__('Password')}}</label>
+              <div class="password-toggle">
+                <input type="password" id="password"  name="password" class="form-control form-control-lg" required>
+                <label class="password-toggle-btn" aria-label="Show/hide password">
+                  <input class="password-toggle-check" name="remember" type="checkbox">
+                  <span class="password-toggle-indicator"></span>
+                </label>
+                <div class="invalid-feedback position-absolute start-0 top-100">Please enter your password!</div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="form-check">
+                <input type="checkbox" id="remember" class="form-check-input">
+                <label for="remember" class="form-check-label fs-base">{{ __('Remember me') }}</label>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary shadow-primary btn-lg w-100"> {{ __('Log in') }}
+            </button>
+          </form>
+          <a href="#" class="btn btn-link btn-lg w-100"> {{ __('Forgot your password?') }}</a>
+          <hr class="my-4">
+          <h6 class="text-center mb-4">Ou connectez-vous avec votre réseau social</h6>
+          <div class="row row-cols-1 row-cols-sm-2">
+            <div class="col mb-3">
+              <a href="#" class="btn btn-icon btn-secondary btn-google btn-lg w-100">
+                <i class="bx bxl-google fs-xl me-2"></i>
+                Google
+              </a>
+            </div>
+            <div class="col mb-3">
+              <a href="#" class="btn btn-icon btn-secondary btn-facebook btn-lg w-100">
+                <i class="bx bxl-facebook fs-xl me-2"></i>
+                Facebook
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+</x-app-layout>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -45,12 +45,57 @@
               </ul>
             </div>
             <div class="offcanvas-footer border-top">
-              <a href="#" class="btn btn-primary w-100" target="_blank" rel="noopener">
+              <a href="{{ route('login')}}" class="btn btn-primary w-100" >
                 <i class="bx bx-user fs-5 lh-1 me-1"></i>
                 &nbsp;Connexion
               </a>
             </div>      
           </div>
+
+          @auth
+          <ul class="navbar-nav  m-2">
+            <li class="nav-item dropdown ">
+              @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                  <img src="{{ Auth::user()->profile_photo_url }}" class="d-block rounded-circle" width="40" alt="{{ Auth::user()->name }}">
+
+                  {{-- <button class="flex text-sm border-2 border-transparent rounded-circle focus:outline-none focus:border-gray-300 transition">
+                      <img class="h-8 w-8 rounded-circle object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="" />
+                  </button> --}}
+              @else
+                  <span class="inline-flex rounded-md">
+                      <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                          {{ Auth::user()->name }}
+
+                          <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                          </svg>
+                      </button>
+                  </span>
+              @endif
+              <ul class="dropdown-menu px-4">
+                <div class="block  py-2 text-xs text-gray-400">
+                    {{ __('Manage Account') }}
+                </div>
+                <li><a href="{{ route('profile.show') }}" class="dropdown-item">{{ __('Profile') }}</a></li>
+               
+                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                <li><a href="{{ route('api-tokens.index') }}" class="dropdown-item">{{ __('API Tokens') }}</a></li>
+                @endif
+
+                <div class="border-t border-gray-100"></div>
+                <li>
+                  <form method="POST" action="{{ route('logout') }}">
+                    @csrf 
+                  <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
+                  this.closest('form').submit();">{{ __('Log Out') }}</a></li>
+                </form>
+                <li>
+              </ul>
+            </li>
+          </ul>
+           
+          @endauth
+
           <div class="form-check form-switch mode-switch pe-lg-1 ms-auto me-4" data-bs-toggle="mode">
             <input type="checkbox" class="form-check-input" id="theme-mode">
             {{-- <label class="form-check-label d-none d-sm-block" for="theme-mode">Light</label> --}}
@@ -59,9 +104,17 @@
           <button type="button" class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <a href="#" class="btn btn-primary btn-sm fs-sm rounded d-none d-lg-inline-flex" target="_blank" rel="noopener">
-            <i class="bx bx-user fs-5 lh-1 me-1"></i>
-            &nbsp;Connexion
-          </a>
+
+          @guest
+            <a href="{{ route('login')}}" class="btn btn-primary btn-sm fs-sm rounded d-none d-lg-inline-flex">
+              <i class="bx bx-user fs-5 lh-1 me-1"></i>
+              &nbsp;Connexion
+            </a>
+          @endguest
+
+          
+          
+        
+          
         </div>
     </header>
