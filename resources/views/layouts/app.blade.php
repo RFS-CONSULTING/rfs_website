100c123052a282174ff1b7724fafece038339b7c
chadrack kanza <chadrackkanza2015@gmail.com>
2022-02-07 12:55:32 +0100

    hidden scroll button bottom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -231,10 +231,10 @@
           </footer>
         </section>
       <!-- Back to top button -->
-      <a href="#top" class="btn-scroll-top" data-scroll>
+      {{-- <a href="#top" class="btn-scroll-top" data-scroll>
         <span class="btn-scroll-top-tooltip text-muted fs-sm me-2">Top</span>
         <i class="btn-scroll-top-icon bx bx-chevron-up"></i>
-      </a>
+      </a> --}}
 
         <!-- Vendor Scripts -->
       <script src="/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
