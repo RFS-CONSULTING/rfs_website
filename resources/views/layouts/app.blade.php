ae9c27566e8a2b5a2847711bfc0eddfa65a1ae45
chadrack kanza <chadrackkanza2015@gmail.com>
2022-02-07 12:32:24 +0100

    messenger plugin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -249,6 +249,34 @@
       <script src="/assets/vendor/parallax-js/dist/parallax.min.js"></script>
       <!-- Main Theme Script -->
       <script src="/assets/js/theme.min.js"></script>
+
+      <!-- Messenger Plugin de discussion Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plugin de discussion code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "103681187982106");
+    chatbox.setAttribute("attribution", "biz_inbox");
+
+    window.fbAsyncInit = function() {
+        FB.init({
+        xfbml            : true,
+        version          : 'v12.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
     </div>
 
     @stack('modals')
